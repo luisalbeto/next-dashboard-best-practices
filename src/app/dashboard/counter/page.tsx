0c9b151484e3431765b1e0bfe13eff39049dd0a7
¨luisalbeto¨ <luis-alberto11@hotmail.com>
2024-07-31 10:39:55 -0400

    Feat: sidebarmenuitem

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,6 +1,20 @@
+import { CarCounter } from "@/app/shopping-car/components/CarCounter"
 
-export default function DashboardPage() {
+
+export const metadata = {
+  title: 'Shopping Car',
+  description: 'Un contador'
+}
+
+export default function CounterPage() {
+
   return(
-    <div>Hello Dashboard</div>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+
+      <CarCounter value={20}/>
+
+     
+    </div>
   )
 }
---
+++ src/app/dashboard/main/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainPage(){
+  return (
+    <div>
+      <h1>Hello Main page</h1>
+    </div>
+  )
+}
+
