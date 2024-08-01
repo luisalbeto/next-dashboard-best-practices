a8353bb64d48a898d139ba188795f8d62a69f2fc
¨luisalbeto¨ <luis-alberto11@hotmail.com>
2024-08-01 14:24:35 -0400

    Feat: Counter reducer with redux

--- src/shopping-car/components/CarCounter.tsx
+++ src/shopping-car/components/CarCounter.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useAppDispatch, useAppSelector } from "@/store";
+import { addOne, substractOne } from "@/store/counter/counterSlice";
 import { useState } from "react"
 
 interface Props {
@@ -7,16 +9,23 @@ interface Props {
 }
 
 export const CarCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value)
+
+  const count = useAppSelector( state => state.counter.count)
+  const dispatch = useAppDispatch()
+ // const [counter, setCounter] = useState(value)
 
   return(
     <>
-      <span className="text-9xl">{counter}</span>
+      <span className="text-9xl">{count}</span>
     <div className="flex">
-    <button onClick={() => setCounter(counter + 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+    <button 
+    onClick={() => dispatch(addOne())} 
+    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
       +1
     </button>
-    <button onClick={() => setCounter(counter -1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+    <button 
+    onClick={() => dispatch(substractOne())} 
+    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
       -1
     </button>
     </div>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  counterReducer  from './counter/counterSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const store = configureStore({
   reducer: {
-    counterReducer,
+    counter: counterReducer,
   },
 })
 
@@ -11,3 +12,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
